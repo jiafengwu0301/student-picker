3d05967dfd9bffb20a24a2280f9aca2cd5059bdb
Abram Hindle <abram.hindle@softwareprocess.es>
2014-09-11 22:57:42 -0600

    Added Test for Randomness

--- src/ca/softwareprocess/studentpicker/test/StudentListTest.java
+++ src/ca/softwareprocess/studentpicker/test/StudentListTest.java
@@ -33,9 +33,33 @@ public class StudentListTest extends TestCase {
 		studentList.removeStudent(testStudent);
 		students = studentList.getStudents();		
 		assertTrue("Student List Size Isn't Small Enough", students.size() == 0);
-		assertFalse("Test Student Still Contained?",students.contains(testStudent));
+		assertFalse("Test Student Still Contained?",students.contains(testStudent));		
 		
-		
+	}
+	public void testChooseStudentList() {
+		StudentList studentList = new StudentList();
+		String studentName = "A Student";
+		Student testStudent = new Student(studentName);
+		studentList.addStudent(testStudent);
+		// Testing for 1 student
+		for (int i = 0 ; i  < 10; i++) {
+			Student student = studentList.chooseStudent();		
+			assertTrue("Student is not null", student != null);
+			assertTrue("Didn't choose the right student",student.equals(testStudent));
+		}
+		String studentNameB = "B Student";
+		Student testStudentB = new Student(studentNameB);
+		studentList.addStudent(testStudentB);
+		Student [] studentArray = { testStudent, testStudentB };
+		for (int i = 0 ; i <  studentArray.length; i++) {
+			Student targetStudent = studentArray[i];
+			int maxcount = 1000;
+			while(maxcount > 0 && !targetStudent.equals(studentList.chooseStudent())) {
+				// do nothing;			
+				maxcount--;
+			}
+			assertTrue("Too Many iterations", maxcount > 0);
+		}
 	}
 
 	
