fefea24270dcd24646e0010f1115cefa1e91039f
Abram Hindle <abram.hindle@softwareprocess.es>
2014-09-11 22:43:07 -0600

    Added Student and StudentList

---
+++ src/ca/softwareprocess/studentpicker/Student.java
@@ -0,0 +1,14 @@
+package ca.softwareprocess.studentpicker;
+
+public class Student {
+	protected String studentName;
+	
+	public Student(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getName() {
+		return this.studentName;
+	}
+	
+}
