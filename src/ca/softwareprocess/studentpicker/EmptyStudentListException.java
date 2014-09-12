c27483a9fdc704cc9f89a581fc914bf0841b8a4d
Abram Hindle <abram.hindle@softwareprocess.es>
2014-09-11 23:10:24 -0600

    Dealt with Empty Student Lists and updated UML and Refactored StudentList

---
+++ src/ca/softwareprocess/studentpicker/EmptyStudentListException.java
@@ -0,0 +1,10 @@
+package ca.softwareprocess.studentpicker;
+
+public class EmptyStudentListException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2417935479828663701L;
+
+}
--- src/ca/softwareprocess/studentpicker/StudentList.java
+++ src/ca/softwareprocess/studentpicker/StudentList.java
@@ -23,9 +23,21 @@ public class StudentList {
 		studentList.remove(testStudent);		
 	}
 
-	public Student chooseStudent() {
+	public Student chooseStudent() throws EmptyStudentListException {
+		int size = studentList.size();
+		if (size <= 0) {
+			throw new EmptyStudentListException();
+		}
 		int index = (int) (studentList.size() * Math.random());
 		return studentList.get(index);
+	}
+
+	public int size() {
+		return studentList.size();
+	}
+
+	public boolean contains(Student testStudent) {
+		return studentList.contains(testStudent);
 	}
 
 }
